7e86aee774e3e0267f471c97164a5fbf55a069c0
FacundoBettella <facundobettella@outlook.com>
2020-12-20 21:08:39 -0300

    CP commiteado por FacundoBettella a las 1608509319274

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -79,13 +79,14 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
   if (numero < 2) return false;
   if (numero === 2) return true;
-  for (var i = 2; i < numero ; i++) {
-    if (numero % i === 0) {
-      return false;
-    }
+  for (var i = 2; i < numero; i++){
+  if(numero % i === 0) {
+   return false;
   }
-  return true; 
+  return true;
+  }
 }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
